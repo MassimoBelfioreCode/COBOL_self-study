c27b59f20a5ef84ffb902028d5e1ef4a09d2c716
MassimoBelfioreCode <uni401047@studium.unict.it>
2025-12-11 23:31:20 +0100

    refactor: replaced LIBRO-REC with a group variable

---
+++ src/group_vars.cbl
@@ -0,0 +1,19 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. GROUP_VARS.
+DATA DIVISION.
+    WORKING-STORAGE SECTION.
+    *> libro così è una group variable, globale e unica all'interno del programma
+        01 LIBRO-GROUP.
+            02 TIT PIC X(20).
+            02 AUT PIC X(20).
+            02 GEN PIC X(20).
+            02 PREZZO PIC 9(2).9(1) VALUE 15.90.
+            
+PROCEDURE DIVISION.
+    DISPLAY "Libro 1".
+    
+    SET AUT TO "Stephen King".
+    SET GEN TO "Horror".
+    SET TIT TO "Pet Sematary".
+    DISPLAY LIBRO-GROUP.
+STOP RUN.
