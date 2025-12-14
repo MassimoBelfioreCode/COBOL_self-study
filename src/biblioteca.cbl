26e0a46a2ef0a4acea14e6452014363599b46744
MassimoBelfioreCode <uni401047@studium.unict.it>
2025-12-14 23:30:04 +0100

    fix: fixed code indentation

--- src/biblioteca.cbl
+++ src/biblioteca.cbl
@@ -1,19 +1,19 @@
 IDENTIFICATION DIVISION.
 PROGRAM-ID. BIBLIOTECA.
 DATA DIVISION.
-    WORKING-STORAGE SECTION.
-    *> dichiarazione di un record libro
-      01 LIBRO-RECORD.
-            02 TIT PIC X(20).
-            02 AUT PIC X(20).
-            02 GEN PIC X(20).
-            02 PREZZO PIC 9(2).9(1) VALUE 15.90.
+WORKING-STORAGE SECTION.
+*> dichiarazione di un record libro
+01 LIBRO-RECORD.
+	02 TIT PIC X(20).
+	02 AUT PIC X(20).
+	02 GEN PIC X(20).
+	02 PREZZO PIC 9(2).9(1) VALUE 15.90.
             
 PROCEDURE DIVISION.
-    DISPLAY "Libro 1".
+	DISPLAY "Libro 1".
     
-    SET AUT TO "Stephen King".
-    SET GEN TO "Horror".
-    SET TIT TO "Pet Sematary".
-    DISPLAY LIBRO-RECORD.
+	SET AUT TO "Stephen King".
+	SET GEN TO "Horror".
+	SET TIT TO "Pet Sematary".
+	DISPLAY LIBRO-RECORD.
 STOP RUN.
