055fc9fde0af85c22977bd9dec3dbcaf309f260b
MassimoBelfioreCode <uni401047@studium.unict.it>
2025-12-16 16:19:19 +0100

    style: fixed code indentation

--- src/biblioteca.cbl
+++ src/biblioteca.cbl
@@ -4,10 +4,10 @@ DATA DIVISION.
      WORKING-STORAGE SECTION.
 * dichiarazione di un record libro
      01 LIBRO-RECORD.
-	02 TIT PIC X(20).
-	02 AUT PIC X(20).
-	02 GEN PIC X(20).
-	02 PREZZO PIC 9(2).9(1) VALUE 15.90.
+          02 TIT PIC X(20).
+          02 AUT PIC X(20).
+          02 GEN PIC X(20).
+          02 PREZZO PIC 9(2).9(1) VALUE 15.90.
             
 PROCEDURE DIVISION.
 	DISPLAY "Libro 1".
